a91c7d73bc9f0c7fbe55f409c25af4d303915f75
iamthepoe <lucagamerbrime@gmail.com>
2023-05-10 17:54:00 -0300

    create server.ts

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const SERVER_PORT = process.env.PORT || 3000;
+const app = express();
+
+app.listen(SERVER_PORT, ()=>{
+    console.log('Server running at http://localhost:' + SERVER_PORT);
+});
+
