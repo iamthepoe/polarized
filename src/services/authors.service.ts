4feae515419e72e1b5f06abadd7640a4f3027c44
iamthepoe <lucagamerbrime@gmail.com>
2023-05-10 20:12:53 -0300

    create method

--- src/services/authors.service.ts
+++ src/services/authors.service.ts
@@ -1,8 +1,24 @@
+import {Author} from '@prisma/client'
 import { authorsClient } from "../clients/clients.prisma";
+import slugify from "slugify";
 
 export class authorsService{
     private client: typeof authorsClient;
     constructor(client: typeof authorsClient){
         this.client = client;
+    }
+
+    async create(data: Author){
+        const {name} = data;
+        const slug = slugify(name);
+
+        const response = await this.client.create({
+            data:{
+                name,
+                slug
+            }
+        });
+
+        return {status: 201, data: response, message: 'Created with success.'};
     }
 }
