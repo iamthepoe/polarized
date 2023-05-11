666f09f33eb6ac9cdf4416bae53328482d448aa5
iamthepoe <lucagamerbrime@gmail.com>
2023-05-11 18:45:10 -0300

    delete route

--- src/routes/authors.routes.ts
+++ src/routes/authors.routes.ts
@@ -25,4 +25,11 @@ router.post('/author', async (req,res)=>{
     return res.status(code).json({data, message});
 });
 
+router.delete('/author/:id', async (req,res)=>{
+    const {id} = req.params;
+    const response = await authorsService.deleteOne(id);
+    const {data, code, message} = response;
+    return res.status(code).json({data, message});
+});
+
 export {router as AuthorsRoutes};
