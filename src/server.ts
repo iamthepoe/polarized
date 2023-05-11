3be3c3257aa99d64420ba2a6568a56d3c64d32fc
iamthepoe <lucagamerbrime@gmail.com>
2023-05-10 21:33:52 -0300

    add express configs

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,8 @@ dotenv.config();
 const SERVER_PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.json());
+
 app.listen(SERVER_PORT, ()=>{
     console.log('Server running at http://localhost:' + SERVER_PORT);
 });
