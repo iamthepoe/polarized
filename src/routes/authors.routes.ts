ea2695355325774db5fd67de5b778c2f71bea494
iamthepoe <lucagamerbrime@gmail.com>
2023-05-10 22:00:02 -0300

    create author routes

--- src/routes/authors.routes.ts
+++ src/routes/authors.routes.ts
@@ -3,3 +3,12 @@ import {authorsServiceFactory} from '../factories/authors.service.factory';
 
 const router = express.Router();
 const authorsService = authorsServiceFactory();
+
+router.post('/author', async (req,res)=>{
+    const {name} = req.body;
+    const response = await authorsService.create(name);
+    const {data, code, message} = response;
+    return res.status(code).json({data, message});
+});
+
+export {router as AuthorsRoutes};
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import { AuthorsRoutes } from './routes/authors.routes';
 
 dotenv.config();
 
@@ -7,6 +8,8 @@ const SERVER_PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+
+app.use('/', AuthorsRoutes);
 
 app.listen(SERVER_PORT, ()=>{
     console.log('Server running at http://localhost:' + SERVER_PORT);
