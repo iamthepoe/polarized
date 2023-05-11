f2b5340b9b24146c84a4bb977234bbe24191199a
iamthepoe <lucagamerbrime@gmail.com>
2023-05-10 22:45:56 -0300

    add json error middleware

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use((err, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if (err instanceof SyntaxError && 'body' in err) {
+        return res.status(400).send({ message: err.message }); // Bad request
+    }
+    next();
+});
+
 app.use('/', AuthorsRoutes);
 
 app.listen(SERVER_PORT, ()=>{
