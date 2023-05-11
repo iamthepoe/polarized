b3688c622b5e8257b419edb1df302a87dde942ac
iamthepoe <lucagamerbrime@gmail.com>
2023-05-10 22:30:59 -0300

    refactoring GET /authors route

--- src/routes/authors.routes.ts
+++ src/routes/authors.routes.ts
@@ -5,7 +5,8 @@ const router = express.Router();
 const authorsService = authorsServiceFactory();
 
 router.get('/authors', async(req,res)=>{
-    const response = await authorsService.findAll();
+    const name = req.query.name as string | undefined;
+    const response = await authorsService.findMany(name);
     const {data, code, message} = response;
     return res.status(code).json({data, message});
 });
