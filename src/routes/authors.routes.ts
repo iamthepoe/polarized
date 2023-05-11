c5e1dd542c3944c33455234a8b8b7a96a424f489
iamthepoe <lucagamerbrime@gmail.com>
2023-05-10 22:12:56 -0300

    add /authors GET route

--- src/routes/authors.routes.ts
+++ src/routes/authors.routes.ts
@@ -4,6 +4,12 @@ import {authorsServiceFactory} from '../factories/authors.service.factory';
 const router = express.Router();
 const authorsService = authorsServiceFactory();
 
+router.get('/authors', async(req,res)=>{
+    const response = await authorsService.findAll();
+    const {data, code, message} = response;
+    return res.status(code).json({data, message});
+});
+
 router.post('/author', async (req,res)=>{
     const {name} = req.body;
     const response = await authorsService.create(name);
