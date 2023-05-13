59501cd72a65952e2a3dba998644b7c07e178a9f
iamthepoe <lucagamerbrime@gmail.com>
2023-05-13 12:24:48 -0300

    add two props to oppositions client

--- src/clients/clients.prisma.ts
+++ src/clients/clients.prisma.ts
@@ -3,6 +3,9 @@ const prisma = new PrismaClient();
 
 const authorsClient = prisma.author;
 const phrasesClient = prisma.phrase;
-const oppositionsClient = prisma.opposition;
+const oppositionsClient = {
+    opposition: prisma.opposition,
+    authors: prisma.author
+};
 
 export {authorsClient, phrasesClient, oppositionsClient};
