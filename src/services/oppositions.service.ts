5d31c4d247bf5f7ed6ea5e2654d30da78689f8a8
iamthepoe <lucagamerbrime@gmail.com>
2023-05-13 12:25:49 -0300

    add create method

--- src/services/oppositions.service.ts
+++ src/services/oppositions.service.ts
@@ -7,4 +7,39 @@ export class OppositionsService{
     constructor(client: typeof oppositionsClient){
         this.client = client;
     }
+
+    async create(data: {firstAuthorId: string, secondAuthorId: string}){
+        const {firstAuthorId, secondAuthorId} = data;
+        try{
+            const authors = await this.client.authors.findMany({
+                where: {
+                    OR: [
+                        {id: firstAuthorId},
+                        {id: secondAuthorId}
+                    ]
+                },
+                select: {
+                    name: true
+                }
+            });
+
+            console.log(authors)
+
+            if (authors.length < 2) return {code: 404, data: null, message: 'One or both authors not found.'};
+            
+            const slug = slugify(`${authors[0].name} ${authors[1].name}`, {lower: true});
+            const response = await this.client.opposition.create({
+                data: {
+                    slug,
+                    firstAuthorId,
+                    secondAuthorId
+                }
+            });
+
+            return {code: 201, data: response, message: 'Created with success.'};
+        } catch {
+            return {code: 500, data: null, message: 'Server internal error.'};
+        }
+    }
+    
 }
