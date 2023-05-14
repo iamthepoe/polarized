294c3160ea077134174c8891353027d8aa5da8e3
iamthepoe <luca1901p@gmail.com>
2023-05-14 14:21:06 -0300

    add GET /oppositions route

--- src/routes/opposition.routes.ts
+++ src/routes/opposition.routes.ts
@@ -4,6 +4,13 @@ import {oppositionsServiceFactory} from '../factories/oppositions.service.factor
 const router = express.Router();
 const oppositionsService = oppositionsServiceFactory();
 
+router.get('/oppositions', async (req,res)=>{
+    const slug = req.query.slug as string | undefined;
+    const response = await oppositionsService.findMany(slug);
+    const {data, code, message} = response;
+    return res.status(code).json({data, message});
+});
+
 router.post('/opposition', async (req,res)=>{
     const {firstAuthorId, secondAuthorId} = req.body;
     const response = await oppositionsService.create({firstAuthorId, secondAuthorId});
